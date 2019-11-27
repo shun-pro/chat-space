2bb5c82fa00bebd546406128cda0b20c9ff754f7
shun-pro <@shun.80367246@gmail.com>
2019-11-27 12:21:36 +0900

    group

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,15 @@ class UsersController < ApplicationController
 
   def edit
   end
+  def index
+  end
 
   def update
     if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
-    end
+    end  
   end
 
   private
