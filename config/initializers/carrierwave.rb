6ddfe1480e08719a311f1981fde8260ef83b7372
shun-pro <@shun.80367246@gmail.com>
2019-12-06 17:17:01 +0900

    deploytest

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'deploytest.shun'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/deploytest.shun'
+  config.fog_directory  = 'deploytestshun'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/deploytestshun'
 end
